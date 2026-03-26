6862a2e275cb073deb39f2f1429f19ba919fa2d8
Sadita06 <Sadita.F.Sadat-1@ou.edu>
2026-03-26 16:16:18 -0500

    added comments to index and main file

---
+++ main.js
@@ -0,0 +1,11 @@
+/*
+Main entry point for WebGL application
+
+Responsibilities:
+- Get WebGL context from canvas
+- Compile shaders (vertex + fragment)
+- Create buffers (geometry data)
+- Set up camera matrices (view + projection)
+- Start render loop
+- Call other modules (player, world, lighting, collision)
+*/
